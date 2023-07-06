c5fb9f4186796af64067892705bcc4e5dc70e985
YuPhe1 <oksunboy1@naver.com>
2023-07-06 11:41:54 +0900

    23.07.06

---
+++ java.sql
@@ -0,0 +1,11 @@
+create table tbl_juso(
+  no int primary key,
+  name varchar(20) not null,
+  juso varchar(500),
+  phone varchar(20)
+);
+
+desc tbl_juso;
+
+drop table tbl_juso;
+
---
+++ students.sql
@@ -0,0 +1,46 @@
+create table students(
+  sno CHAR(6) NOT NULL PRIMARY KEY, /* 학번 */
+  sname VARCHAR(20) NOT NULL, /* 이름 */
+  dept VARCHAR(20) NOT NULL,  /* 학과 */
+  birthday date,              /* 생년월일 */
+  year INT DEFAULT 1         /* 학년 */
+);
+
+desc STUDENTS;
+
+drop table students;
+
+insert into students(sno, sname, dept) 
+values ('202301', '홍길동', '컴정');
+insert into students(sno, sname, dept, birthday) 
+values ('202302', '심청이', '컴정', '2000-10-04');
+insert into students(sno, sname, dept, birthday) 
+values ('202303', '이순신', '컴정', '2000-12-17');
+
+select *
+from students;
+
+select *
+from students
+where sno = '202302';
+
+SELECT *
+FROM STUDENTS
+WHERE SNAME like '%이%';
+
+SELECT *
+FROM STUDENTS
+WHERE SNAME like '%이%'
+ORDER by sname, birthday;
+
+SELECT max(sno) + 1 as newno
+from STUDENTS;
+
+UPDATE STUDENTS 
+SET BIRTHDAY = '2000-01-01'
+WHERE SNO = '202301';
+
+DELETE FROM STUDENTS
+WHERE SNO = '202301';
+
+commit;
